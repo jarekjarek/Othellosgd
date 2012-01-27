7dc27459a7c1bc59d8ab06854bf9e446c23cdd74
Jarek Gryczko <jarek.gryczko@gmail.com>
2012-01-27 03:18:31 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+#define kolor 12
+
+// plansza
+const int wiersze = 8;  
+const int kolumny = 8;
+
+// pionki
+const char czlowiek    = 'X';
+const char computer = 'O';
+
+
