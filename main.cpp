367aa161ce3748f86ee64f26e65384c5d7a04b06
Jarek Gryczko <jarek.gryczko@gmail.com>
2012-01-27 03:46:52 +0100

    Trzeci commit.....

--- main.cpp
+++ main.cpp
@@ -58,3 +58,56 @@ int Mechanizm(char tablica[][kolumny], int wiersz, int kolumna, char zeton)
         if (tablica[wiersz][c] == zeton)
             pionki_do_przewrocenia = c;
     }
+    // upewniamy sie ze nasz pionek jest co najmiej dwa pola dalej aby odwrócic przeciwnika
+      if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia > kolumna + 1)
+      {
+
+          //odwrócenie w prawo pionków przewiwnika
+          for (int c = kolumna + 1; c < pionki_do_przewrocenia; c++)
+          {
+              tablica[wiersz][c] = zeton;
+              odwrocone_pionki++;
+          }
+      }
+
+
+      // czy mamy pionki w lewo aby odwrócic od podanej lokalizacji?
+      pionki_do_przewrocenia = -1;
+      for (int c = kolumna - 1; c >= 0 && tablica[wiersz][c] != ' ' && pionki_do_przewrocenia == -1; c--)
+      {
+          if (tablica[wiersz][c] == zeton)
+              pionki_do_przewrocenia = c;
+      }
+
+
+
+
+      if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia < kolumna - 1)
+      {
+
+          for (int c = kolumna - 1; c > pionki_do_przewrocenia; c--)
+          {
+              tablica[wiersz][c] = zeton;
+              odwrocone_pionki++;
+          }
+      }
+
+
+       //czy mamy pionki w góre aby odwrócic od podanej lokalizacji?
+      pionki_do_przewrocenia = -1;
+      for (int r = wiersz - 1; r >= 0 && tablica[r][kolumna] != ' ' && pionki_do_przewrocenia == -1; r--)
+      {
+          if (tablica[r][kolumna] == zeton)
+              pionki_do_przewrocenia = r;
+      }
+
+      if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia < wiersz - 1)
+      {
+
+          for (int r = wiersz - 1; r > pionki_do_przewrocenia; r--)
+          {
+              tablica[r][kolumna] = zeton;
+              odwrocone_pionki++;
+          }
+      }
+
