b885105973c09b869c78e869bf4f845b696cf54c
Jarek Gryczko <jarek.gryczko@gmail.com>
2012-01-27 05:02:31 +0100

    Czwarty commit ....

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <windows.h>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
 using namespace std;
 
 
 #define kolor 12
 
-// plansza
+// planszas
 const int wiersze = 8;  
 const int kolumny = 8;
 
 // pionki
 const char czlowiek    = 'X';
 const char computer = 'O';
+
 
 /****************************************************************************/
 
@@ -58,56 +62,176 @@ int Mechanizm(char tablica[][kolumny], int wiersz, int kolumna, char zeton)
         if (tablica[wiersz][c] == zeton)
             pionki_do_przewrocenia = c;
     }
+
+
     // upewniamy sie ze nasz pionek jest co najmiej dwa pola dalej aby odwrócic przeciwnika
-      if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia > kolumna + 1)
-      {
+    if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia > kolumna + 1)
+    {
 
-          //odwrócenie w prawo pionków przewiwnika
-          for (int c = kolumna + 1; c < pionki_do_przewrocenia; c++)
-          {
-              tablica[wiersz][c] = zeton;
-              odwrocone_pionki++;
-          }
-      }
-
-
-      // czy mamy pionki w lewo aby odwrócic od podanej lokalizacji?
-      pionki_do_przewrocenia = -1;
-      for (int c = kolumna - 1; c >= 0 && tablica[wiersz][c] != ' ' && pionki_do_przewrocenia == -1; c--)
-      {
-          if (tablica[wiersz][c] == zeton)
-              pionki_do_przewrocenia = c;
-      }
+        //odwrócenie w prawo pionków przewiwnika
+        for (int c = kolumna + 1; c < pionki_do_przewrocenia; c++)
+        {
+            tablica[wiersz][c] = zeton;
+            odwrocone_pionki++;
+        }
+    }
 
 
+    // czy mamy pionki w lewo aby odwrócic od podanej lokalizacji?
+    pionki_do_przewrocenia = -1;
+    for (int c = kolumna - 1; c >= 0 && tablica[wiersz][c] != ' ' && pionki_do_przewrocenia == -1; c--)
+    {
+        if (tablica[wiersz][c] == zeton)
+            pionki_do_przewrocenia = c;
+    }
 
 
-      if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia < kolumna - 1)
-      {
-
-          for (int c = kolumna - 1; c > pionki_do_przewrocenia; c--)
-          {
-              tablica[wiersz][c] = zeton;
-              odwrocone_pionki++;
-          }
-      }
 
 
-       //czy mamy pionki w góre aby odwrócic od podanej lokalizacji?
-      pionki_do_przewrocenia = -1;
-      for (int r = wiersz - 1; r >= 0 && tablica[r][kolumna] != ' ' && pionki_do_przewrocenia == -1; r--)
-      {
-          if (tablica[r][kolumna] == zeton)
-              pionki_do_przewrocenia = r;
-      }
+    if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia < kolumna - 1)
+    {
 
-      if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia < wiersz - 1)
-      {
+        for (int c = kolumna - 1; c > pionki_do_przewrocenia; c--)
+        {
+            tablica[wiersz][c] = zeton;
+            odwrocone_pionki++;
+        }
+    }
 
-          for (int r = wiersz - 1; r > pionki_do_przewrocenia; r--)
-          {
-              tablica[r][kolumna] = zeton;
-              odwrocone_pionki++;
-          }
-      }
 
+     //czy mamy pionki w góre aby odwrócic od podanej lokalizacji?
+    pionki_do_przewrocenia = -1;
+    for (int r = wiersz - 1; r >= 0 && tablica[r][kolumna] != ' ' && pionki_do_przewrocenia == -1; r--)
+    {
+        if (tablica[r][kolumna] == zeton)
+            pionki_do_przewrocenia = r;
+    }
+
+    if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia < wiersz - 1)
+    {
+
+        for (int r = wiersz - 1; r > pionki_do_przewrocenia; r--)
+        {
+            tablica[r][kolumna] = zeton;
+            odwrocone_pionki++;
+        }
+    }
+
+
+    // //czy mamy pionki w dó³ aby odwrócic od podanej lokalizacji?
+    pionki_do_przewrocenia = -1;
+    for (int r = wiersz + 1; r < wiersze && tablica[r][kolumna] != ' ' && pionki_do_przewrocenia == -1; r++)
+    {
+        if (tablica[r][kolumna] == zeton)
+            pionki_do_przewrocenia = r;
+    }
+
+
+    if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia > wiersz + 1)
+    {
+
+        for (int r = wiersz + 1; r < pionki_do_przewrocenia; r++)
+        {
+            tablica[r][kolumna] = zeton;
+            odwrocone_pionki++;
+        }
+    }
+
+
+     //czy mamy pionki w góre-lewo aby odwrócic od podanej lokalizacji?
+    pionki_do_przewrocenia = -1;
+    int c = kolumna - 1;
+    for (int r = wiersz - 1; c >= 0 && r >= 0 && tablica[r][c] != ' ' && pionki_do_przewrocenia == -1; r--)
+    {
+        if (tablica[r][c] == zeton)
+            pionki_do_przewrocenia = r;
+        c--;
+    }
+
+
+    if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia < wiersz - 1)
+    {
+        c = kolumna - 1;
+        for (int r = wiersz - 1; r > pionki_do_przewrocenia; r--)
+        {
+            tablica[r][c] = zeton;
+            odwrocone_pionki++;
+            c--;
+        }
+    }
+
+
+     //czy mamy pionki w góre-prawo aby odwrócic od podanej lokalizacji?
+    pionki_do_przewrocenia = -1;
+    c = kolumna + 1;
+    for (int r = wiersz - 1; c < kolumny && r >= 0 && tablica[r][c] != ' ' && pionki_do_przewrocenia == -1; r--)
+    {
+        if (tablica[r][c] == zeton)
+            pionki_do_przewrocenia = r;
+        c++;
+    }
+
+
+    if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia < wiersz - 1)
+    {
+        c = kolumna + 1;
+        for (int r = wiersz - 1; r > pionki_do_przewrocenia; r--)
+        {
+            tablica[r][c] = zeton;
+            odwrocone_pionki++;
+            c++;
+        }
+    }
+
+
+     //czy mamy pionki w dó³-lewo aby odwrócic od podanej lokalizacji?
+    pionki_do_przewrocenia = -1;
+    c = kolumna - 1;
+    for (int r = wiersz + 1; c >= 0 && r < wiersze && tablica[r][c] != ' ' && pionki_do_przewrocenia == -1; r++)
+    {
+        if (tablica[r][c] == zeton)
+            pionki_do_przewrocenia = r;
+        c--;
+    }
+
+
+    if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia > wiersz + 1)
+    {
+        c = kolumna - 1;
+        for (int r = wiersz + 1; r < pionki_do_przewrocenia; r++)
+        {
+            tablica[r][c] = zeton;
+            odwrocone_pionki++;
+            c--;
+        }
+    }
+
+
+     //czy mamy pionki w dó³-prawo aby odwrócic od podanej lokalizacji?
+    pionki_do_przewrocenia = -1;
+    c = kolumna + 1;
+    for (int r = wiersz + 1; c < kolumny && r < wiersze && tablica[r][c] != ' ' && pionki_do_przewrocenia == -1; r++)
+    {
+        if (tablica[r][c] == zeton)
+            pionki_do_przewrocenia = r;
+        c++;
+    }
+
+
+    if (pionki_do_przewrocenia != -1 && pionki_do_przewrocenia > wiersz + 1)
+    {
+        c = kolumna + 1;
+        for (int r = wiersz + 1; r < pionki_do_przewrocenia; r++)
+        {
+            tablica[r][c] = zeton;
+            odwrocone_pionki++;
+            c++;
+        }
+    }
+
+    // Jesli zadnych pionkow nie da sie przewrocic jest to nielegalny ruch
+    if (odwrocone_pionki == 0)
+        tablica[wiersz][kolumna] = ' ';
+
+    return odwrocone_pionki;
+}
